424af6d50207cf89bfe37d6d70fc9a0493e7e121
Beritha Niyotwagira <b.niyotwagi@alustudent.com>
2023-02-03 08:08:18 -0800

    Python code

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    print('{}'.format(chr(i)), end="")
+def print_low_alpha_ex():
+    for n in range(97, 123):
+        if n != 101 and n != 113:
+            print('{}'.format(chr(n)), end='')
+if __name__ == '__main__':
+    print_low_alpha_ex()
+
