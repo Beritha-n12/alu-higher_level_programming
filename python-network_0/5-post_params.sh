f44d4945c9e338e3034832bc144c8990d3dd8ae7
Niyotwagira Beritha <b.niyotwagi@alustudent.com>
2023-04-05 11:52:13 +0000

    python network

--- python-network_0/5-post_params.sh
+++ python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # a Bash script that sends a POST request with 2 variables
-curl -s -L -X POST "$1" -d "email=hr%40holbertonschool%2Ecom&subject=I+will+always+be+here+for+PLD"
+curl -s -L -X POST "$1" -d "email=test@gmail.com&subject=I+will+always+be+here+for+PLD"
