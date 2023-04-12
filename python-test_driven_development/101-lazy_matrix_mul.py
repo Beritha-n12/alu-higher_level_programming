0c4c67d666df6cb932ef1cdc8b02057b37ecf96d
Beritha <b.niyotwagi@alustudent.com>
2023-04-12 04:18:59 -0700

    #advanced

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
+"""
+lazy matrix multiplication using module
+"""
 import numpy as np
 
 
 def lazy_matrix_mul(m_a, m_b):
+    """
+    mulltiplying matrices
+    checking TypeError with unittest
+    """
     return np.matmul(m_a, m_b)
