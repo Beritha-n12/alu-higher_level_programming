13cd66219ce7573ac79bc767988661e285c755b2
Beritha <b.niyotwagi@alustudent.com>
2023-04-12 04:01:55 -0700

    print square

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,24 +1,16 @@
 #!/usr/bin/python3
+"""
+    prints a square
+    takes in the size
+"""
 
 
 def print_square(size):
-    """
-    prints a square of given size
-    unit tests are located in tests/4-print_square.txt
-    """
-    if not isinstance(size, (int, float)):
+    square = ""
+    if not isinstance(size, int):
         raise TypeError("size must be an integer")
-    elif size < 0:
-        if isinstance(size, float):
-            raise TypeError("size must be an integer")
-        else:
-            raise ValueError("size must be >= 0")
-
-    size = int(size)  # if it was a float convert it
-    i = 0
-
-    for i in range(0, size):
-        j = 0
-        for j in range(0, size):
-            print('#', end='')
-        print()
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for c in range(size):
+        square += "#" * size + "\n"
+    print(square, end="")
