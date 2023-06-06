da20a82307124bf1a601bdc54d6d3d76e067893a
Beritha-n12 <116920809+Beritha-n12@users.noreply.github.com>
2023-06-06 12:00:51 +0000

    task2

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
+const args = process.argv.slice(2);
 
-const beri = process.argv.length[2]; //exclude two statements
-
-if (beri.length === 0){
-    console.log("No argument");
-} else if (beri.length === 1);{
-    console.log("Argument found");
-} else (beri.length === 3) {
-    console.log("Arguments found");
+if (args.length === 0) {
+  console.log('No argument');
+} else if (args.length === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
