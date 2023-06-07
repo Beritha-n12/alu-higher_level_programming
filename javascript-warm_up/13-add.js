acb7d4111c5874e73599b0b316b2cd3861b7bbb2
Beritha-n12 <116920809+Beritha-n12@users.noreply.github.com>
2023-06-07 12:47:57 +0000

    task13

--- javascript-warm_up/13-add.js
+++ javascript-warm_up/13-add.js
@@ -1,8 +1,5 @@
 #!/usr/bin/node
-
-function added(a, b) {
-  return a + b;
-}
-
-result = added(3, 5);
-console.log(result); // output: 8
+exports.add = function (a, b) {
+    return (a + b);
+  };
+  
