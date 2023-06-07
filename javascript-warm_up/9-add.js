9e3678754260ed8113c4d80a7ee79d23fcc8dfdd
Beritha-n12 <116920809+Beritha-n12@users.noreply.github.com>
2023-06-07 10:45:16 +0000

    task9

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 
-function add(a,b){
+function add(a,b) {
   return a + b
 }
 
-const berr1 = parseInt(process.argv[2]);
-const berr2 = parseInt(process.argv[3]);
+const args1 = parseInt(process.argv[2]);
+const args2 = parseInt(process.argv[3]);
 
-if (isNaB(berr1) || isNaB(berr2)){
-    console.log('NaB');
+if (isNaN(args1) || isNaN(args2)) {
+    console.log('NaN');
 } else {
     const result = add(berr1, berr2);
     console.log(result);
