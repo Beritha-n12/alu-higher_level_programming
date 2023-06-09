f1fe33a4ea29dbab0db24e342ab4ae1b73d98e48
Beritha-n12 <116920809+Beritha-n12@users.noreply.github.com>
2023-06-09 11:50:31 +0000

    task

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -1,12 +1,12 @@
 #!/usr/bin/Node
 module.exports = class Rectangle {
-    constructor(w, h) {
-        if (w > 0 && h > 0) {
-      }
-  
-      this.width = w;
-      this.height = h;
+  constructor(w, h) {
+    if (w > 0 && h > 0) {
     }
+  
+  this.width = w;
+  this.height = h;
+  }
   
     print() {
       if (this.width < 0 || this.height < 0) {
