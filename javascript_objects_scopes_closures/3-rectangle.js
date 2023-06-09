94ba1cf4cccba365a158686ed2bf4300552bdbd2
Beritha-n12 <116920809+Beritha-n12@users.noreply.github.com>
2023-06-09 12:48:35 +0000

    task4

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
 class Rectangle {
-    constructor (w, h) {
-      if (w <= 0 || h <= 0) {
-        return {};
-      }
-      this.width = w;
-      this.height = h;
+  constructor (w, h) {
+    if (w <= 0 || h <= 0) {
+      return {};
     }
-  
+    this.width = w;
+    this.height = h;
+  }
     print () {
       for (let i = 0; i < this.height; i++) {
         let row = '';
