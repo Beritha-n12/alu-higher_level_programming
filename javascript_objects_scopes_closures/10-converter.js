4be989dbdfba2e97404938809daa3a3a5c869786
Beritha-n12 <116920809+Beritha-n12@users.noreply.github.com>
2023-06-09 14:34:02 +0000

    task10

--- javascript_objects_scopes_closures/10-converter.js
+++ javascript_objects_scopes_closures/10-converter.js
@@ -1,9 +1,6 @@
 #!/usr/bin/node
-exports.esrever = function (list = []) {
-    const newList = [];
-    for (let i = list.length; i >= 0; i--) {
-      newList.push(list[i]);
-    }
-    newList.shift();
-    return newList;
+exports.converter = function (base) {
+  return function(number) {
+    return number.toString(base);
+  };
 };
