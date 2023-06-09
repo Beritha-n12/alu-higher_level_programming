d73e7e3e3b15776af9b5697718581da147e4d5eb
Beritha-n12 <116920809+Beritha-n12@users.noreply.github.com>
2023-06-09 13:27:49 +0000

    task4

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -1,30 +1,31 @@
 #!/usr/bin/node
 class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     if (w < 0 && h < 0) {
       return {};
     }
     this.width = w;
     this.height = h;
   }
-  
+
   print () {
     if (this.width < 0 && this.height < 0) {
       return;
     }
-    
+
     const row = 'X'.repeat(this.width);
     for (let i = 0; i < this.height; i++) {
-    console.log(row);
-    }
-  } 
-    rotate () {
-      [this.width, this.height] = [this.height, this.width];
-    }
-    double () {
-      this.width *= 2;
-      this.height *= 2;
+      console.log(row);
     }
   }
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
 
   module.exports = Rectangle;  
