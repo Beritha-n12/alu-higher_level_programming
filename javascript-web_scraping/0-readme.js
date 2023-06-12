adf75039403b6f517bfc52dfe003973fa50bb26c
Beritha-n12 <116920809+Beritha-n12@users.noreply.github.com>
2023-06-12 09:14:36 +0000

    tasko

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 function readFile (filePath) {
   fs.readFile(filePath, 'utf-8', (err, data) => {
     if (err) {
-      console.error (err);
+      console.error(err);
     } else {
       console.log(data);
     }
@@ -12,7 +12,7 @@ function readFile (filePath) {
 
 if (process.argv.length < 3) {
   console.log('Please provide the file path as an argument.');
-  } else {
+} else {
   const filePath = process.argv[2];
   readFile(filePath);
 }
