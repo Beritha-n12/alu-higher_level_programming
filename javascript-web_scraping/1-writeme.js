afc11dff7940046790c35690014db4ab9d4d45f3
Beritha-n12 <116920809+Beritha-n12@users.noreply.github.com>
2023-06-12 09:47:45 +0000

    task1

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,20 +1,11 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-function writeFile(filePath, content) {
-  fs.writeFile(filePath, content, { encoding: 'utf-8' }, (err) => {
-    if (err) {
-      console.error(err);
-    } else {
-      console.log('Written File');
-    }
-  });
-}
+const filePath = process.argv[2];
+const content = process.argv[3];
 
-if (process.argv.length < 4) {
-  console.log(filepath);
-} else {
-  const filePath = process.argv[2];
-  const content = process.argv[3];
-  writeFile(filePath, content);
-}
+fs.writeFile(filePath, content, 'utf-8', (err) => {
+  if (err) {
+    console.error(err);
+  }
+});
