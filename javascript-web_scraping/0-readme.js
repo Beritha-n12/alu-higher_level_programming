0c445ee605c020bc78c656756b316f422a8050df
Beritha-n12 <116920809+Beritha-n12@users.noreply.github.com>
2023-06-12 09:04:02 +0000

    task0

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-function readFile(filePath) {
+function readFile (filePath) {
   fs.readFile(filePath, 'utf-8', (err, data) => {
     if (err) {
-      console.error(`Error occurred while reading the file: ${err}`);
+      console.error (`Error occurred: ${err}`);
     } else {
       console.log(data);
     }
