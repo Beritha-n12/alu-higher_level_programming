56efe0e8debab40df4d6c6de61a5f44329f9c655
Beritha-n12 <116920809+Beritha-n12@users.noreply.github.com>
2023-06-12 09:09:44 +0000

    task0

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 function readFile (filePath) {
   fs.readFile(filePath, 'utf-8', (err, data) => {
     if (err) {
-      console.error (error);
+      console.error (err);
     } else {
       console.log(data);
     }
