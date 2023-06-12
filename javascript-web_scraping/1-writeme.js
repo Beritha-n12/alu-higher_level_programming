ef77aaa444624bd52d9ece3fec82c760de100fa0
Beritha-n12 <116920809+Beritha-n12@users.noreply.github.com>
2023-06-12 09:29:03 +0000

    task1

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,21 +1,20 @@
 #!/usr/bin/node
+const fs = require('fs');
 
-const request = require('request');
-const path = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`;
-
-request(path, function (error, response, body) {
-  if (error) {
-    console.error(error);
-  } else {
-    const result = JSON.parse(body);
-    for (const item of result.characters) {
-      request(item, function (error, response, body) {
-        if (error) {
-          console.error(error);
-        } else {
-          console.log(JSON.parse(body).name);
-        }
-      });
+function writeFile(filePath, content) {
+  fs.writeFile(filePath, content, 'utf-8', (err) => {
+    if (err) {
+      console.error(`Error occurred while writing to the file: ${err}`);
+    } else {
+      console.log('File written successfully.');
     }
-  }
-});
+  });
+}
+
+if (process.argv.length < 4) {
+  console.log('Please provide the file path and content as arguments.');
+} else {
+  const filePath = process.argv[2];
+  const content = process.argv[3];
+  writeFile(filePath, content);
+}
